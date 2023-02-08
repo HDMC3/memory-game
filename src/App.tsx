19b494654d474d341a7644cf569ee4fdf07ef716
Hugo Moran <moranh56@gmail.com>
2023-02-08 09:14:25 -0600

    Add principal pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { Switch, Route, Redirect } from 'wouter';
+import { Game } from './pages/Game';
+import { Home } from './pages/Home';
+
 export const App = () => {
     return (
         <div>
-            <h1>App</h1>
+            <Switch>
+                <Route path="/" component={Home} />
+                <Route path="/game" component={Game} />
+                <Route>{() => <Redirect to='/' /> }</Route>
+            </Switch>
         </div>
     );
 };
