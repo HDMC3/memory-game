a93ff58d46e07db7e207cf8b242cdf4b320dc99f
Hugo Moran <moranh56@gmail.com>
2023-02-11 19:09:39 -0600

    Add level banner component

---
+++ src/components/level-banner/LevelBanner.css.ts
@@ -0,0 +1,50 @@
+import { style } from '@vanilla-extract/css';
+import { vars } from '../../App.css';
+
+export const levelBannerContainer = style({
+    border: `4px solid ${vars.color.basic_contrast}`,
+    borderRadius: 10,
+    overflow: 'hidden'
+});
+
+export const levelBannerTitle = style({
+    margin: 0,
+    padding: '0.5rem 0',
+    textAlign: 'center',
+    width: '100%',
+    borderBottom: `4px solid ${vars.color.basic_contrast}`
+});
+
+export const itemsContainer = style({
+    display: 'grid',
+    gridTemplateColumns: '1fr 1fr',
+    gridTemplateRows: '1fr 1fr',
+    gap: '0.7rem',
+    padding: '1rem',
+    columnGap: '1.5rem'
+});
+
+export const playButton = style({
+    border: `3px solid ${vars.color.basic_contrast}`,
+    background: vars.color.basic_2,
+    color: vars.color.text.basic,
+    fontWeight: 'bold',
+    fontSize: '1.5rem',
+    transition: 'box-shadow 0.3s',
+    borderRadius: 5,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    boxShadow: `3px 3px 0 0 ${vars.color.basic_contrast}`,
+    ':active': {
+        boxShadow: 'none !important'
+    },
+    ':hover': {
+        cursor: 'pointer'
+    }
+});
+
+export const playIcon = style({
+    transition: 'fill 0.3s',
+    fill: vars.color.basic_contrast
+});
---
+++ src/components/level-banner/LevelBanner.tsx
@@ -0,0 +1,54 @@
+import { motion } from 'framer-motion';
+import { type PropsWithChildren } from 'react';
+import { vars } from '../../App.css';
+import { playButton, itemsContainer, levelBannerContainer, levelBannerTitle, playIcon } from './LevelBanner.css';
+
+interface Props {
+    levelName: string
+    levelBg: string
+    levelColor: string
+}
+
+export const LevelBanner = ({ children, levelName, levelBg, levelColor }: PropsWithChildren<Props>) => {
+    return (
+
+        <section className={ levelBannerContainer }>
+            <motion.h2
+                style={{ backgroundColor: levelBg, color: levelColor }}
+                className={ levelBannerTitle }
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+            >
+                { levelName }
+            </motion.h2>
+
+            <div className={ itemsContainer }>
+
+                { children }
+
+                <motion.button
+                    className={ playButton }
+                    animate={{
+                        x: [100, 0],
+                        opacity: [0, 0.5, 1],
+                        boxShadow: [
+                            `0 0 0 0 ${vars.color.basic_contrast}`,
+                            `0 0 0 0 ${vars.color.basic_contrast}`,
+                            `3px 3px 0 0 ${vars.color.basic_contrast}`
+                        ]
+                    }}
+                    transition={{
+                        duration: 0.6,
+                        times: [0.1, 0.95, 1],
+                        ease: 'easeInOut'
+                    }}
+                >
+                    <svg className={playIcon} width="44" height="44" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                        <path d="M7 4v16l13 -8z" />
+                    </svg>
+                </motion.button>
+            </div>
+        </section>
+    );
+};
