e1d31dffae4b76f322ff04c6e39695f0a114fb99
Hugo Moran <moranh56@gmail.com>
2023-02-13 19:04:46 -0600

    Add game stats banner

---
+++ src/pages/Game.css.ts
@@ -0,0 +1,69 @@
+import { style } from '@vanilla-extract/css';
+import { vars } from '../App.css';
+
+export const gameContainer = style({
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    gap: '1rem',
+    padding: '1rem 0.5rem'
+});
+
+export const statsContainer = style({
+    border: '4px solid',
+    display: 'flex',
+    alignItems: 'center',
+    borderRadius: 10,
+    maxWidth: '500px',
+    width: '100%',
+    flexWrap: 'wrap',
+    boxSizing: 'border-box',
+    padding: '0.5rem 0.5rem',
+    boxShadow: `2px 4px 0 0 ${vars.color.basic_contrast}`,
+    position: 'relative'
+});
+
+export const levelButton = style({
+    position: 'absolute',
+    top: -10,
+    left: -10
+});
+
+export const stat = style({
+    boxSizing: 'border-box',
+    fontWeight: 'bold',
+    display: 'flex',
+    flexWrap: 'wrap',
+    fontSize: '1.25rem',
+    gap: '1rem',
+    justifyContent: 'center',
+    padding: '0.5rem 0.5rem',
+    textAlign: 'center',
+    width: '100%'
+});
+
+export const moveStat = style({
+    borderBottom: '2px solid',
+    paddingBottom: '1rem'
+});
+
+export const timeStat = style({
+    borderTop: '2px solid',
+    paddingTop: '1rem'
+});
+
+export const titleStat = style({
+    display: 'flex',
+    alignItems: 'center',
+    margin: 0
+});
+
+export const statValue = style({
+    fontFamily: 'monospace',
+    fontSize: '1.1rem',
+    border: `3px solid ${vars.color.basic_contrast}`,
+    padding: '0.2rem 0.6rem',
+    borderRadius: 5,
+    fontWeight: 'bold',
+    backgroundColor: vars.color.basic_4
+});
--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,7 +1,35 @@
+import { Button } from '../components/button/Button';
+import { ThemeButton } from '../components/theme-button/ThemeButton';
+import { gameContainer, statValue, stat, statsContainer, titleStat, timeStat, moveStat, levelButton } from './Game.css';
+
 export const Game = () => {
     return (
-        <>
-            <h1>Game page</h1>
-        </>
+        <div className={gameContainer}>
+            <div className={statsContainer}>
+
+                <ThemeButton />
+                <Button color='success' className={levelButton}>
+                    Facil
+                </Button>
+
+                <div className={[stat, moveStat].join(' ')}>
+                    <h5 className={titleStat}>
+                        Movimientos
+                    </h5>
+                    <span className={statValue}>
+                        19
+                    </span>
+                </div>
+
+                <div className={[stat, timeStat].join(' ')}>
+                    <h5 className={titleStat}>
+                        Tiempo
+                    </h5>
+                    <span className={statValue}>
+                        01:35
+                    </span>
+                </div>
+            </div>
+        </div>
     );
 };
