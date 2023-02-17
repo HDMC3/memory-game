ec5705622ea19fbfb8e8f69a97b032779aa99aa4
Hugo Moran <moranh56@gmail.com>
2023-02-16 18:57:16 -0600

    Add cards actions

--- src/context/game.tsx
+++ src/context/game.tsx
@@ -20,12 +20,42 @@ export const GameProvider = ({ children }: PropsWithChildren) => {
         dispatch({ type: GameActionKind.CHANGE_LEVEL, payload: newLevel });
     };
 
+    const revealCard = (cardId: number) => {
+        const { activeCard, cards } = state;
+
+        dispatch({ type: GameActionKind.REVEAL_CARD, payload: { cardId } });
+
+        if (!activeCard) {
+            dispatch({ type: GameActionKind.SET_ACTIVE_CARD, payload: { cardId } });
+            return;
+        }
+
+        dispatch({ type: GameActionKind.START_CHECKING_MOVE });
+
+        const card = cards[cardId];
+
+        if (activeCard.pokemon.id === card.pokemon.id) {
+            dispatch({ type: GameActionKind.INCREASE_MOVES });
+            dispatch({ type: GameActionKind.SET_DONE_CARDS, payload: { cardId } }); // dentro de esta accion se podria establecer la carta activa a null
+            dispatch({ type: GameActionKind.REMOVE_ACTIVE_CARD });
+            return;
+        }
+
+        setTimeout(() => {
+            dispatch({ type: GameActionKind.INCREASE_MOVES });
+            dispatch({ type: GameActionKind.HIDE_CARDS }); // dentro de esta accion se podria establece la carta activa a null y checkingMove a false
+            dispatch({ type: GameActionKind.REMOVE_ACTIVE_CARD });
+            dispatch({ type: GameActionKind.END_CHECKING_MOVE });
+        }, 1000);
+    };
+
     return (
         <GameContext.Provider value={{
             state,
             increaseMoves,
             restartMoves,
-            changeGameLevel
+            changeGameLevel,
+            revealCard
         }}
         >
             { children }
--- src/reducers/gameReducer.ts
+++ src/reducers/gameReducer.ts
@@ -1,21 +1,49 @@
+import { type Card } from '../types/card';
+
 export const enum GameActionKind {
     INCREASE_MOVES = 'INCREASE_MOVES',
     RESTART_MOVES = 'RESTART_MOVES',
-    CHANGE_LEVEL = 'CHANGE_LEVEL'
+    CHANGE_LEVEL = 'CHANGE_LEVEL',
+    SET_CARDS = 'SET_CARDS',
+    REVEAL_CARD = 'REVEAL_CARD',
+    SET_ACTIVE_CARD = 'SET_ACTIVE_CARD',
+    REMOVE_ACTIVE_CARD = 'REMOVE_ACTIVE_CARD',
+    SET_DONE_CARDS = 'SET_DONE_CARDS',
+    HIDE_CARDS = 'HIDE_CARDS',
+    START_CHECKING_MOVE = 'START_CHECKING_MOVE',
+    END_CHECKING_MOVE = 'END_CHECKING_MOVE'
 }
 
 export interface GameState {
     movesCount: number
     level: 'ease' | 'medium' | 'hard'
+    checkingMove: boolean
+    cards: Card[]
+    activeCard: Card | null
 }
 
 export type GameAction =
-    { type: GameActionKind.INCREASE_MOVES | GameActionKind.RESTART_MOVES }
-    | { type: GameActionKind.CHANGE_LEVEL, payload: 'ease' | 'medium' | 'hard' };
+    { type: GameActionKind.SET_CARDS, payload: Card[] }
+    | {
+        type: GameActionKind.INCREASE_MOVES
+        | GameActionKind.RESTART_MOVES
+        | GameActionKind.START_CHECKING_MOVE
+        | GameActionKind.END_CHECKING_MOVE
+        | GameActionKind.HIDE_CARDS
+        | GameActionKind.REMOVE_ACTIVE_CARD
+    }
+    | { type: GameActionKind.CHANGE_LEVEL, payload: 'ease' | 'medium' | 'hard' }
+    | { type: GameActionKind.REVEAL_CARD, payload: { cardId: number } }
+    | { type: GameActionKind.SET_ACTIVE_CARD, payload: { cardId: number } }
+    | { type: GameActionKind.SET_DONE_CARDS, payload: { cardId: number } }
+;
 
 export const initialState: GameState = {
     movesCount: 0,
-    level: 'ease'
+    level: 'ease',
+    checkingMove: false,
+    cards: [],
+    activeCard: null
 };
 
 export const gameReducer = (state: GameState, action: GameAction): GameState => {
@@ -36,6 +64,85 @@ export const gameReducer = (state: GameState, action: GameAction): GameState => 
             return {
                 ...state,
                 level: action.payload
+            };
+
+        case GameActionKind.SET_CARDS:
+            return {
+                ...state,
+                cards: action.payload
+            };
+
+        case GameActionKind.REVEAL_CARD:
+            return {
+                ...state,
+                cards: state.cards.map(card => {
+                    if (card.id === action.payload.cardId) {
+                        return {
+                            ...card,
+                            status: 'reveal'
+                        };
+                    }
+                    return card;
+                })
+            };
+
+        case GameActionKind.SET_ACTIVE_CARD:
+            return {
+                ...state,
+                activeCard: { ...state.cards[action.payload.cardId] }
+            };
+
+        case GameActionKind.REMOVE_ACTIVE_CARD:
+            return {
+                ...state,
+                activeCard: null
+            };
+
+        case GameActionKind.SET_DONE_CARDS: {
+            const { activeCard } = state;
+            const { cardId } = action.payload;
+
+            return {
+                ...state,
+                cards: state.cards.map(card => {
+                    if (card.id === cardId || activeCard?.id === cardId) {
+                        return {
+                            ...card,
+                            status: 'done'
+                        };
+                    }
+
+                    return card;
+                })
+            };
+        }
+        case GameActionKind.HIDE_CARDS:
+            return {
+                ...state,
+                cards: state.cards.map(card => {
+                    if (card.status === 'reveal') {
+                        return {
+                            ...card,
+                            status: 'hidden'
+                        };
+                    }
+
+                    return {
+                        ...card
+                    };
+                })
+            };
+
+        case GameActionKind.START_CHECKING_MOVE:
+            return {
+                ...state,
+                checkingMove: true
+            };
+
+        case GameActionKind.END_CHECKING_MOVE:
+            return {
+                ...state,
+                checkingMove: false
             };
 
         default:
