b818d5b79aa60afe02beac0f37528ed871e3875c
Hugo Moran <moranh56@gmail.com>
2023-02-16 20:41:40 -0600

    Add game provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Switch, Route, Redirect } from 'wouter';
+import { GameProvider } from './context/game';
 import { Game } from './pages/Game';
 import { Home } from './pages/Home';
 
@@ -6,7 +7,11 @@ export const App = () => {
     return (
         <Switch>
             <Route path="/" component={Home} />
-            <Route path="/game" component={Game} />
+            <Route path="/game">
+                <GameProvider>
+                    <Game />
+                </GameProvider>
+            </Route>
             <Route>{() => <Redirect to='/' /> }</Route>
         </Switch>
     );
