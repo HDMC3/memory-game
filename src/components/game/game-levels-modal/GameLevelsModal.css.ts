2bb7b77383928098c521249100db1cc973e1554b
Hugo Moran <moranh56@gmail.com>
2023-02-17 16:28:27 -0600

    Add modal to select level

---
+++ src/components/game/game-levels-modal/GameLevelsModal.css.ts
@@ -0,0 +1,57 @@
+import { style } from '@vanilla-extract/css';
+import { vars } from '../../../App.css';
+
+export const modalContainer = style({
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    height: '100vh',
+    backgroundColor: '#64646460',
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    zIndex: 1
+});
+
+export const modal = style({
+
+    width: '100%',
+    backgroundColor: vars.color.basic_1,
+    border: `4px solid ${vars.color.basic_contrast}`,
+    borderBottomWidth: 0,
+    alignSelf: 'flex-end',
+    boxShadow: 'none',
+    borderRadius: '10px 10px 0 0',
+    zIndex: 2,
+    '@media': {
+        'screen and (min-width: 500px)': {
+            borderBottomWidth: '4px',
+            maxWidth: '300px',
+            borderRadius: 10,
+            boxShadow: '2px 4px 0 0',
+            alignSelf: 'flex-start',
+            marginTop: '4rem'
+        }
+    }
+});
+
+export const modalHeader = style({
+    borderBottom: `4px solid ${vars.color.basic_contrast}`,
+    width: '100%',
+    padding: '0.75rem 0'
+});
+
+export const modalTitle = style({
+    margin: 0,
+    textAlign: 'center',
+    fontWeight: 'bold'
+});
+
+export const modalBody = style({
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '1rem',
+    padding: '1.5rem 0',
+    alignItems: 'center'
+});
---
+++ src/components/game/game-levels-modal/GameLevelsModal.tsx
@@ -0,0 +1,69 @@
+import { type MouseEvent, useEffect } from 'react';
+import { useGame } from '../../../hooks/useGame';
+import { type GameLevel } from '../../../reducers/gameReducer';
+import { Button } from '../../ui/button/Button';
+import { modal, modalBody, modalContainer, modalHeader, modalTitle } from './GameLevelsModal.css';
+
+interface Props {
+    open: boolean
+    onClose: () => void
+}
+
+export const GameLevelsModal = ({ open, onClose }: Props) => {
+    const { changeGameLevel } = useGame();
+
+    useEffect(() => {
+        document.body.style.overflowY = open ? 'hidden' : 'auto';
+    }, [open]);
+
+    const handleBackdropClick = (e: MouseEvent<HTMLDivElement>) => {
+        const { classList } = e.target as HTMLDivElement;
+        if (classList.contains(modalContainer)) {
+            onClose();
+        }
+    };
+
+    const handleSelectLevel = (newLevel: GameLevel) => {
+        changeGameLevel(newLevel);
+        onClose();
+    };
+
+    return (
+        <>
+            {
+                // eslint-disable-next-line multiline-ternary, operator-linebreak
+                !open ? null :
+                <div className={modalContainer} onClick={handleBackdropClick}>
+                    <div className={modal}>
+                        <div className={modalHeader}>
+                            <h2 className={modalTitle}>Selecciona dificultad</h2>
+                        </div>
+                        <div className={modalBody}>
+                            <Button
+                                onClick={() => handleSelectLevel({ name: 'ease', cardsNumber: 12 })}
+                                color='success'
+                            >
+                                Facil
+                            </Button>
+
+                            <Button
+                                onClick={() => handleSelectLevel({ name: 'medium', cardsNumber: 20 })}
+                                color='warning'
+                            >
+                                Medio
+                            </Button>
+
+                            <Button
+                                onClick={() => handleSelectLevel({ name: 'hard', cardsNumber: 30 })}
+                                color='error'
+                            >
+                                Dificil
+                            </Button>
+                        </div>
+                    </div>
+                </div>
+            }
+
+        </>
+    );
+};
